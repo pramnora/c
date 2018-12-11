8a68fe29cd207630d662c5f666d39ef305d1d1f8
Paul Ramnora <pramnora@yahoo.com>
2018-12-11 16:33:41 +0000

    Create hw01.c
    Created file: hw01.c/a simple 'Hello, world!' program

---
+++ hw01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* NOTE:  The compiler I'm using is: CodeBlocks */
+
+int main()
+{
+    printf("Hello world!\n");
+    printf("%d",1+1);
+    return 0;
+}
